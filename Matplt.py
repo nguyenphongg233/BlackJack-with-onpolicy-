45d99e20e506b322b2ee6c9cb1c74fa0d20affb6
Nguyễn Phong <94354349+nguyenphongg233@users.noreply.github.com>
2025-03-25 18:32:23 +0700

    Update Matplt.py

--- Matplt.py
+++ Matplt.py
@@ -10,14 +10,12 @@ x, y, xx, yy = data.T
 
 plt.subplot(1,2,1)
 plt.scatter(x, y)
-plt.grid()
 plt.xlabel("Dealer's first card")
 plt.ylabel("Player's current point")
 plt.title("No usable ACE")
 
 plt.subplot(1,2,2)
 plt.scatter(xx, yy)
-plt.grid()
 plt.xlabel("Dealer's first card")
 plt.ylabel("Player's current point")
 plt.title("usable ACE")
